9ca7fe4bc087f2701b9704e76472030433c1bddf
saul.leyva <saul.leyva@MEXL006217.AT.local>
2024-02-21 14:54:56 -0600

    method creation

---
+++ Uno/src/Aqui/DemoMethod.java
@@ -0,0 +1,20 @@
+package Aqui;
+
+public class DemoMethod {
+
+	public static void main(String[] args) {
+	
+		//execution here
+		DemoMethod demo = new DemoMethod();
+		demo.getData();
+		DemoMethod2 demo2 = new DemoMethod2();
+		demo2.getData();
+
+	}
+
+	//method here
+	public void getData()
+	{
+		System.out.println("hello world");
+	}
+}
---
+++ Uno/src/Aqui/DemoMethod2.java
@@ -0,0 +1,13 @@
+package Aqui;
+
+public class DemoMethod2 {
+	//only the method here
+	//no method´s execution
+	
+	public void getData()
+	{
+		System.out.println("hello world method 2");
+	}
+
+	
+}
